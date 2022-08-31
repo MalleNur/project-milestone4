8d97d41da24cd081b8dd4c24454e2ab6131088f4
MalleNur <maljunnur92@gmail.com>
2022-08-31 02:35:38 +0000

    finished admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Book, Meetup
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    '''
+    Define the Book fields shown in the admin panel.
+    '''
+    list_display = (
+        'title', 'author', 'year_published', 'members_rating', 'has_been_read'
+    )
+
+
+@admin.register(Meetup)
+class MeetupAdmin(admin.ModelAdmin):
+    '''
+    Define the Meetup fields shown in the admin panel.
+    '''
+    list_display = ('long_title', 'meetup_date', 'book1')
