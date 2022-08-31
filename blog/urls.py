7f474476e266cfa8194ae321fad210676b1201e2
MalleNur <maljunnur92@gmail.com>
2022-08-31 03:50:47 +0000

    change + add urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,16 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_bag, name='view_bag'),
-    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
-    path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
-    path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
+    path('', views.MeetupList.as_view(), name='blog-home'),
+    path('meetups/new/', views.CreateMeetup.as_view(), name='meetup-create'),
+    path('meetups/<int:pk>/', views.MeetupDetail.as_view(), name='meetup_detail'),
+    path('meetups/<int:pk>/update', views.UpdateMeetup.as_view(), name='meetup-update'),
+    path('meetups/<int:pk>/delete', views.DeleteMeetup.as_view(), name='meetup-delete'),
+    path('library/', views.BookList.as_view(), name='book-list'),
+    path('books/new/', views.CreateBook.as_view(), name='book-create'),
+    path('books/<int:pk>/update', views.UpdateBook.as_view(), name='book-update'),
+    path('books/<int:pk>/delete', views.DeleteBook.as_view(), name='book-delete'),
+    path('comments/<int:pk>/delete', views.DeleteComment.as_view(), name='comment-delete'),
+    path('about/', views.about, name='blog-about'),
 ]
