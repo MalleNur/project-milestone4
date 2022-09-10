e8e8be4f860407062a77c210321fc54122362891
MalleNur <maljunnur92@gmail.com>
2022-09-10 10:44:16 +0000

    fixes

--- bookcircle/settings.py
+++ bookcircle/settings.py
@@ -45,6 +45,10 @@ if DEVELOPMENT:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+
 else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
